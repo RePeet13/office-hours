cf01427b4fdc8f8ebf69b4bf4f51f2cb0befeb3d
Taylor Peet <repeet13@gmail.com>
2013-04-22 18:39:45 -0400

    stripped page bare, added in base components to update as necessary

--- meteor/office-hours/office-hours.js
+++ meteor/office-hours/office-hours.js
@@ -1,5 +1,7 @@
 if (Meteor.isClient) {
-
+	// TODO implement a changing color based on the age of the last update
+	
+	
 	// Static configuration type things
 	Accounts.ui.config({
 		passwordSignupFields: 'USERNAME_ONLY'
@@ -8,15 +10,11 @@ if (Meteor.isClient) {
 	//Startup Function
 	Meteor.startup(function() {
 		console.log("entered startup");
+		/*
 		Session.set("page", "login");
-		
 		var fragment = Meteor.render(function () {
 			console.log("rendering template fragment");
 			var currentpage = Session.get("page");
-			if (currentpage == "login") {
-				console.log("currentpage: " + currentpage);
-				return Template.login();
-			}
 			if (currentpage == "pick") {
 				console.log("currentpage: " + currentpage);
 				return Template.pick();
@@ -31,23 +29,16 @@ if (Meteor.isClient) {
 		document.body.appendChild(fragment);
 		
 		Session.set("page", "login");
+		*/
 	});
 	
-	// Helper functions
-	function notloggedin() {
-		document.body.appendChild(Template.notloggedin());
+	Deps.autorun{function () {
+	
 	}
-	
-	
+
 	// Event handling
-	Template.navlogin.events({
-		'click' : function () {
-			document.getElementById(Session.get("page")+"li").className = "";
-			document.getElementById("loginli").className = "active";
-			Session.set("page", "login");
-		}
-	});
 	
+	/*
 	Template.navplace.events({
 		'click' : function () {
 			if (Meteor.user() == null){
@@ -59,33 +50,7 @@ if (Meteor.isClient) {
 			Session.set("page", "place");
 		}
 	});
-	
-	Template.navpick.events({
-		'click' : function () {
-			if (Meteor.user() == null){
-				notloggedin();
-				return;
-			}
-			document.getElementById(Session.get("page")+"li").className = "";
-			document.getElementById("pickli").className = "active";
-			Session.set("page", "pick");
-		}
-	});
-	
-	
-//Below this line autogen'ed with meteor
-//TODO take out eventually
-  Template.hello.greeting = function () {
-    return "Welcome to inventory-picker.";
-  };
-
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+	*/
 }
 
 if (Meteor.isServer) {

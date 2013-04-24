d7233a17073cfe03cd011c98c81ac3ff6f7c5864
Taylor Peet <repeet13@gmail.com>
2013-04-24 16:39:56 -0400

    working on getting database call to work, investigating demos, prepare for large change

--- meteor/office-hours/office-hours.js
+++ meteor/office-hours/office-hours.js
@@ -19,22 +19,29 @@ if (Meteor.isClient) {
 		Session.set("option",2);
 		
 		var frag = Meteor.render(function () {
+		
+			
 			var opt = Session.get("option");
 			console.log("fetching");
-			var myloc = Update.find().fetch();
-			console.log("updating");
-			Update.update(myloc._id, {option: opt});
-			console.log("option " + opt);
+			var myloc = Update.find({}, {name: "Location"});
+			console.log("myloc: " + myloc.count());
+			console.log("option: " + opt + ", myloc.area: ");// + myloc[0].area);
+			/*
 			switch(opt) {
 				case 0:
 					//case where you display whatever the db says
 					break;
 				case 1:
+					return Template.status({area: "Off-Campus"});
+				case 2:
 					return Template.status({area: "Baird Lab"});
 				default:
 					return Template.status({area: "My Office (Klaus)"});
 			}
+			*/
+			return Template.status({area: myloc.area});
 			});
+		frag = Template.status({area: "loading"});
 		document.body.appendChild(frag);
 	});
 	
@@ -52,14 +59,36 @@ if (Meteor.isClient) {
 	
 	Template.status.events({
 		'click .btn-large' : function () {
-			console.log("ouch, you clicked me! " + Meteor.user().username);
-			if (Meteor.user().username == "repeet13" || Meteor.user().username == "RePeet13") {
+			console.log("ouch, you clicked me");
+			var dbg = "null";
+			if (Meteor.user() != null)
+				dbg = Meteor.user().username;
+			if (Meteor.user() != null || Meteor.user().username == "repeet13" || Meteor.user().username == "RePeet13") {
 				var opt = Session.get("option");
 				if (opt > 2) {
-					Session.set("option", 1);
+					opt = 0;
 				} else {
-					Session.set("option", opt+1);
+					opt += 1;
 				}
+				
+				var setmyloc = Update.find();
+				console.log("updating: " + myloc.option + " to: " + opt);
+				
+				switch(opt) {
+				case 0:
+					Update.update(setmyloc._id, {option: opt, area: "Off-Campus"});
+					break;
+				case 1:
+					Update.update(setmyloc._id, {option: opt, area: "My Office (Klaus)"});
+					break;
+				case 2:
+					Update.update(setmyloc._id, {option: opt, area: "Baird Lab"});
+					break;
+				default:
+					Update.update(setmyloc._id, {option: opt, area: "My Office (Klaus)"});
+					break;
+				}
+				
 			}
 			Deps.flush();
 		}
@@ -70,11 +99,17 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
 	Meteor.startup(function () {
 		if (Update.find().count() === 0) {
-			Update.insert({name: "Location", option: 2, custom: "none"});
+			Update.insert({name: "Location", option: 0, area: "Off-Campus", date: ""});
 		}
 	});
   
 	Meteor.publish("update", function () {
 		return Update.find({name: "Location"});
+	});
+	
+	Update.allow({
+		insert: function (userId, location) {
+			return false;
+		}
 	});
 }

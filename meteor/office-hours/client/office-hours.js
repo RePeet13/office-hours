0ca9be987d2f6e14faf2ad833f212ac9a68394c3
OpenShift guest <repeet13@gmail.com>
2013-04-25 14:28:13 -0400

    added regen functionality to client

--- meteor/office-hours/client/office-hours.js
+++ meteor/office-hours/client/office-hours.js
@@ -99,31 +99,16 @@ Template.status.events({
 
 Template.regen.events({
     'click .btn-large' : function () {
-		console.log("ouch, you clicked me");
-
-        var opt = 0;
+		console.log("ouch, you clicked my regen");
+        
 		if (checkUser()) { // can probably do this restriction on the server (safer too)
-			opt = (Session.get("option") > 2) ? 0 : opt+=1; //inc if not too big
-			var timestamp = (new Date()).getTime();
-			
-			console.log("updating to: " + opt);
-			Session.set("option", opt);
-			switch(opt) {
-			case 0:
-				Update.update(this._id, {option: opt, area: "Off-Campus", date: timestamp});
-				break;
-			case 1:
-				Update.update(this._id, {option: opt, area: "My Office (Klaus)", date: timestamp});
-				break;
-			case 2:
-				Update.update(this._id, {option: opt, area: "Baird Lab", date: timestamp});
-				break;
-			default:
-				Update.update(this._id, {option: opt, area: "My Office (Klaus)", date: timestamp});
-				break;
-			}
-			
+			regen();
 		}
 	}
 });
 
+// Shared Methods
+
+Meteor.methods({
+  regen: function () {}
+});

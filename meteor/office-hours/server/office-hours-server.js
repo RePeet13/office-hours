bf99b95d3d129155fa4ffdeb901a542d7be1e724
OpenShift guest <repeet13@gmail.com>
2013-04-25 13:08:16 -0400

    changed so database has date object

--- meteor/office-hours/server/office-hours-server.js
+++ meteor/office-hours/server/office-hours-server.js
@@ -2,7 +2,7 @@ Update = new Meteor.Collection("update");
 
 Meteor.startup(function () {
 	if (Update.find().count() === 0) {
-		var timestamp = (new Date()).getTime();
+		var timestamp = new Date();
 		Update.insert({name: "Location", option: 0, area: "Off-Campus", date: timestamp});
 	}
 });

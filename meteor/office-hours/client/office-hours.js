a4ebc8f5693cb4f2cafb9551ae4b78f699fa4e12
OpenShift guest <repeet13@gmail.com>
2013-04-25 14:13:18 -0400

    centralized user checking

--- meteor/office-hours/client/office-hours.js
+++ meteor/office-hours/client/office-hours.js
@@ -26,6 +26,13 @@ Accounts.ui.config({
 	passwordSignupFields: 'USERNAME_ONLY'
 });
 
+checkUser = function () {
+    if (Meteor.user() == null)
+        return false;
+        
+    if (Meteor.user().username == "repeet13" || Meteor.user().username == "RePeet13")
+        return true;
+}
 
 // Template Methods and Helpers
 Template.status.status = function () {
@@ -54,17 +61,48 @@ Template.status.age = function () {
 	return "success";
 }
 
+Template.regen.allowed = function () {
+    return checkUser();
+}
+
 // Event handling
 
 Template.status.events({
 	'click .btn-large' : function () {
 		console.log("ouch, you clicked me");
-		var dbg = "null";
-		if (Meteor.user() != null)
-			dbg = Meteor.user().username;
 		
         var opt = 0;
-		if (Meteor.user() != null && (Meteor.user().username == "repeet13" || Meteor.user().username == "RePeet13")) { // can probably do this restriction on the server (safer too)
+		if (checkUser()) { // can probably do this restriction on the server (safer too)
+			opt = (Session.get("option") > 2) ? 0 : opt+=1; //inc if not too big
+			var timestamp = (new Date()).getTime();
+			
+			console.log("updating to: " + opt);
+			Session.set("option", opt);
+			switch(opt) {
+			case 0:
+				Update.update(this._id, {option: opt, area: "Off-Campus", date: timestamp});
+				break;
+			case 1:
+				Update.update(this._id, {option: opt, area: "My Office (Klaus)", date: timestamp});
+				break;
+			case 2:
+				Update.update(this._id, {option: opt, area: "Baird Lab", date: timestamp});
+				break;
+			default:
+				Update.update(this._id, {option: opt, area: "My Office (Klaus)", date: timestamp});
+				break;
+			}
+			
+		}
+	}
+});
+
+Template.regen.events({
+    'click .btn-large' : function () {
+		console.log("ouch, you clicked me");
+
+        var opt = 0;
+		if (checkUser()) { // can probably do this restriction on the server (safer too)
 			opt = (Session.get("option") > 2) ? 0 : opt+=1; //inc if not too big
 			var timestamp = (new Date()).getTime();
 			
